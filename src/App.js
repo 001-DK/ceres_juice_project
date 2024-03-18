b2fa57795764646a0faf014fa01d2056a64eb573
ShadeF <samanthashade83@gmail.com>
2024-03-18 20:58:40 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+      {/* <LoginForm /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/RegisterForm.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../RegisterForm.css";
+
+function RegisterForm() {
+  return (
+    <div className="container">
+      <h2>Welcome to Ceres Juice Bar</h2>
+      <h3>Register user details</h3>
+      <form>
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input type="text" id="fullName" name="fullName" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" required />
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number</label>
+          <input type="tel" id="phoneNumber" name="phoneNumber" required />
+        </div>
+        <div>
+          <label htmlFor="location">Location</label>
+          <input type="text" id="location" name="location" required />
+        </div>
+        <button type="submit">Register</button>
+        <div className="footer">
+          <p>
+            <a href="register.html">Back To Cashier Login</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default RegisterForm;
